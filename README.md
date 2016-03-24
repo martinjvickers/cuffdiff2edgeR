f904ef28fe22cad0a1804e1cbf95dcd95c7f7bdb
martinjvickers <rudigarude@googlemail.com>
2016-03-24 15:12:03 +0000

    Changed array to hash in order to preserve replicate placement in the columns as cuffdiff does not guarantee or preserve order but edgeR requires it.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # cuffdiff2edgeR
 Perl Script to convert CUFFDIFF output for use with edgeR and Trinity DE analysis
 
+##Rationale
+
+The Trinity Differential Analysis pipeline written in R produces some fantastic heatmaps and other visual representation which is catered for in the CUFFLINKS cummeRbund library. For this reason, this script converts the results of your diff_cuff directory (namely the `genes.read_group_tracking` file) to a format which is readable by edgeR in the Trinity DE analysis tools.
+
 ##Usage:
 
 This script assumes that you have completed the whole cufflinks protocol and have a directory called `diff_out`. Within the `diff_out` directory run the following;
@@ -8,10 +12,3 @@ This script assumes that you have completed the whole cufflinks protocol and hav
 `perl cuffdiff_2_edgeR.pl genes.read_group_tracking > cuffdiff_genes.counts.matrix`
 
 The `cuffdiff_genes.counts.matrix` file can now be used with the Trinity Differential Expression Analysis scripts.
-
-##TODO:
-
-* Ensure reps are in the correct order as CUFFDIFF doesn't seem to guarentee replicate order.
-* Rather than outputting to screen, use perl to write correct output files
-+ Trinity DE analysis assumes that the matrix file format is of the form Trinity_genes.count.matrix
-
--- cuffdiff_2_edgeR.pl
+++ cuffdiff_2_edgeR.pl
@@ -3,26 +3,15 @@ use strict;
 use warnings;
 
 my $line_no = 0;
-my $current_id = "";
 my $line;
 my $filename = $ARGV[0];
-my @row;
-my @header;
-my $first_run = 1;
-my $never_header = 1;
+my $current_id = "";
+my %conditions = ();
+my $first_run = 0;
+my @headers = "";
+my $first_print = 0;
 
 open (INFILE, $filename) or die "Sorry, cannot open the file titled: $filename";
-
-
-
-#WARNING, the script assumes that the cuffdiff file is always 
-###the way to do this properly;
-#read first N number of lines until there is a change in tracking_id. This will give you the header, the number of samples and then the reps.
-#Create an index so we know which array element a specific condition and rep belongs in the row
-#start reading the file again and this time match the condition and rep with the row array element to ensure that it's always correct.
-
-
-
 
 for $line (<INFILE>) {
 
@@ -42,57 +31,68 @@ for $line (<INFILE>) {
 		($effective_length eq "effective_length") and \\
 		($status eq "status") or \\
 		die "The first line of your genes.read_group_tracking file is incorrect.\nIt should be in the following format as produced by CUFFDIFF:\n\ntracking_id\tcondition\treplicate\traw_frags\tinternal_scaled_frags\texternal_scaled_frags\tFPKM\teffective_length\tstatus\n";
-	
-		$line_no++;
-		next;
 	}
 
-	#now process the results
-	if($line_no != 0){
+	#now create out header array
+	if($line_no != 0)
+	{
 
-		##this is a new gene, so new row with first column containing tracking_id
-		if($current_id ne $tracking_id){
+		#start of a new ID
+		if($tracking_id ne $current_id)
+		{
 
-			#if we've never printed the header but have completed a first run, print header now
-                        if($never_header == 1 && $first_run == 0){
-                                $never_header = 0;
-                                print join("\t", @header), "\n";
-                        }
+			if($first_run != 0)
+			{
 
-			#if we're on our first run, add initial commit
-			if($first_run == 1){
-	                        push(@header, "");
-	                        push(@header, "$condition-$replicate");
-				$first_run = 0;
+				if ($first_print == 0)
+				{
+					my $value = 1;
+					my $numkeys = keys( %conditions );
+					print "\t";
+					foreach my $key (keys %conditions)
+					{
+						if($value < $numkeys)
+						{
+                                        		print "$key\t";
+						} else {
+							print "$key";
+						}
+						$value++;
+                                	}
+					print "\n";
+					$first_print = 1;
+				}
+
+				print "$current_id\t";
+				my $value = 1;
+				my $numkeys = keys( %conditions );
+				
+				foreach my $key (keys %conditions)
+				{
+					if($value < $numkeys)
+					{
+						print "$conditions{$key}\t";
+					} else {
+						print "$conditions{$key}";
+					}
+					$value++;
+				}
+				print "\n";
+			} elsif ($first_run == 0)
+			{
+				$first_run = 1;
 			}
 
-                        if($never_header == 0 && $first_run == 0){
-				print join("\t", @row), "\n";
-                        }
-			
-			@row=(); 
 			$current_id = $tracking_id;
-			push(@row, $current_id);
-			push(@row, $raw_frags);
+			$conditions{ "$condition-$replicate" } = $raw_frags;
 
-			next;
+		} else {
+			$conditions{ "$condition-$replicate" } = $raw_frags;
 		}
-
-		##same gene as before, so append $raw_frags
-		if($current_id eq $tracking_id){
-			push(@row, $raw_frags);
-			if($never_header == 1){
-				push(@header, "$condition-$replicate");
-			}
-		}
-
+	
 	}
 
 	$line_no++;
-
 }
-
-#final line out
-print join("\t", @row), "\n";
 
 close INFILE;
